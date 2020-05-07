e40ccd4498df7243d57808bc58c14dbb14c35cd9
YusnierDixson <yusnierdixson@gmail.com>
2020-05-07 16:14:48 +0200

    inicio crud

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,6 +33,7 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
+                        <li class="nav-item"><a href="{{ route('post.index') }}">Posts</a></li>
 
                     </ul>
 
--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,13 +1,18 @@
-@extends('welcome')
+@extends('layouts.app')
 @section('content')
-@foreach ($posts as $post)
-<div class="card mb-4" style="width: 18rem;">
-   <div class="card-body">
-     <h5 class="card-title">{{ $post->title }}</h5>
+<div class="container">
+    @foreach ($posts as $post)
+    <div class="card mb-4" style="width: 18rem;">
+       <div class="card-body">
+         <h5 class="card-title">{{ $post->title }}</h5>
 
-     <p class="card-text">{{ $post->content }}</p>
+         <p class="card-text">{{ $post->content }}</p>
 
-   </div>
-@endforeach
-{{ $posts->links() }}
+       </div>
+    </div>
+    @endforeach
+    {{ $posts->links() }}
+
+</div>
+
 @endsection
